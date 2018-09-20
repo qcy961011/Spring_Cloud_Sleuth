6c192f269263ddd21b6025ccfa0c9c20213f1199
Qiao-PC <qcy961011>
2018-09-20 21:01:06 +0800

    增加无用信息，需要修改

--- another_zipkin_server/src/main/java/com/qiao/another_zipkin_server/AnotherZipkinServerApplication.java
+++ another_zipkin_server/src/main/java/com/qiao/another_zipkin_server/AnotherZipkinServerApplication.java
@@ -1,10 +1,9 @@
-package com.qiao.zipkin_server;
+package com.qiao.another_zipkin_server;
 
 import brave.sampler.Sampler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,28 +14,31 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
-public class ZipkinServerApplication {
+public class AnotherZipkinServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZipkinServerApplication.class, args);
+        SpringApplication.run(AnotherZipkinServerApplication.class, args);
     }
 
-    private  static final Logger log = Logger.getLogger(ZipkinServerApplication.class.getName());
+    private static final Logger log = Logger.getLogger(AnotherZipkinServerApplication.class.getName());
 
     @Autowired
     private RestTemplate restTemplate;
 
-
-    @RequestMapping("/hi")
-    public String callHome(){
-        log.log(Level.INFO,"calling trace zipkinServer");
-        return restTemplate.getForObject("http://localhost:8989/info",String.class);
+    @Bean
+    public RestTemplate RestTemplate(){
+        return new RestTemplate();
     }
 
-    @RequestMapping("/info")
+    @RequestMapping("/hi")
+    public String home(){
+        log.log(Level.INFO,"hi is being called");
+        return "hi i'm another";
+    }
+
     public String info(){
-        log.log(Level.INFO,"calling trace zipkinServer");
-        return "i'm zipkin_server";
+        log.log(Level.INFO,"info is being called");
+        return restTemplate.getForObject("http://localhost:8988",String.class);
     }
 
     @Bean
@@ -44,10 +46,5 @@ public class ZipkinServerApplication {
         return Sampler.ALWAYS_SAMPLE;
     }
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
 
 }
