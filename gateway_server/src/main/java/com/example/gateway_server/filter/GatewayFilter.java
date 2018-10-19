fa415a5140ef22dc10659b31a268689ebe246305
Qiao-PC <qcy961011>
2018-10-19 20:56:02 +0800

    增加无用信息，需要修改

---
+++ gateway_server/src/main/java/com/example/gateway_server/filter/GatewayFilter.java
@@ -0,0 +1,30 @@
+package com.example.gateway_server.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GatewayFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 900;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
